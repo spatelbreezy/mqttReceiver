fd9ed802f3ea7072e0cc0edaecd8e490cea5762d
Sahil Patel <sahildpatel396@gmail.com>
2024-07-20 14:18:51 -0400

    working for localhost and topic: scada

--- receiver.py
+++ receiver.py
@@ -4,8 +4,16 @@ broker_address = "127.0.0.1" #localhost
 broker_port = 1883
 topic = "scada" #topic to subscribe too
 
-client = mqtt.Client()
-client.connect(broker_address, broker_port)
-client.subscribe(topic)
+def on_message(client, userdata, message):
+    print(f"Received a message: {message.payload.decode('utf-8')} on topic {message.topic}")
 
-client.loop_forever()
+client = mqtt.Client() #Initialize client
+
+client.on_message = on_message #Define function to be called when receiving message
+
+client.connect(broker_address, broker_port) #Connect to the broker
+
+client.subscribe(topic) #Subscribe to the predefined topic
+
+client.loop_forever() #Receive messages
+
